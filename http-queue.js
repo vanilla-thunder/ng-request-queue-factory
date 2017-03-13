b97067b347c5ad1645d59b626dd6fc549a2b34b5
Marat <vanilla-thunder@users.noreply.github.com>
2017-03-13 12:20:23 +0100

    Update http-queue.js

--- http-queue.js
+++ http-queue.js
@@ -1,51 +1,49 @@
 (function () {
     'use strict';
-    angular.module('http-queue', [])
-        .factory('qGet', queueGet)
-        .factory('qPost', queuePost);
+    angular.module('http-queue',[])
+        .factory('qGet', function ($q, $http) {
+            var queue = [],
+                execNext = function () {
+                    var task = queue[0];
+                    $http.get(task.url).then(
+                        function (data) {
+                            //console.log(Date.now() + " GET  >> " + task.url);
+                            queue.shift();
+                            task.d.resolve(data);
+                            if (queue.length > 0) execNext();
+                        },
+                        function (err) {
+                            task.d.reject(err);
+                        });
+                };
+            return function (url) {
+                var d = $q.defer();
+                queue.push({url: url, d: d});
+                if (queue.length === 1) execNext();
+                return d.promise;
+            };
 
-    function queueGet() {
-        var queue = [],
-            execNext = function () {
-                var task = queue[0];
-                $http.get(task.url).then(
-                    function (data) {
-                        //console.log(Date.now() + " GET  >> " + task.url);
-                        queue.shift();
-                        task.d.resolve(data);
-                        if (queue.length > 0) execNext();
-                    },
-                    function (err) {
-                        task.d.reject(err);
-                    });
+        })
+        .factory('qPost', function ($q, $http) {
+            var queue = [],
+                execNext = function () {
+                    var task = queue[0];
+                    $http.post(task.url, task.data).then(
+                        function (data) {
+                            //console.log(Date.now() + " POST >> " + task.url);
+                            queue.shift();
+                            task.d.resolve(data);
+                            if (queue.length > 0) execNext();
+                        },
+                        function (err) {
+                            task.d.reject(err);
+                        });
+                };
+            return function (url, data) {
+                var d = $q.defer();
+                queue.push({url: url, data: data, d: d});
+                if (queue.length === 1) execNext();
+                return d.promise;
             };
-        return function ($q, $http) {
-            var d = $q.defer();
-            queue.push({url: url, d: d});
-            if (queue.length === 1) execNext();
-            return d.promise;
-        };
-    }
-    function queuePost() {
-        var queue = [],
-            execNext = function () {
-                var task = queue[0];
-                $http.post(task.url, task.data).then(
-                    function (data) {
-                        //console.log(Date.now() + " POST >> " + task.url);
-                        queue.shift();
-                        task.d.resolve(data);
-                        if (queue.length > 0) execNext();
-                    },
-                    function (err) {
-                        task.d.reject(err);
-                    });
-            };
-        return function ($q, $http) {
-            var d = $q.defer();
-            queue.push({url: url, data: data, d: d});
-            if (queue.length === 1) execNext();
-            return d.promise;
-        };
-    }
+        });
 })();
